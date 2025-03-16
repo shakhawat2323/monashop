1a86f76210de337d175408b6f726c5320f4cf6f0
shakhawat2323 <shakhawathossain208@gmail.com>
2025-03-16 12:55:23 +0600

    side bar section done

--- src/commonent/Productpage/Productslider/Productlisting.jsx
+++ src/commonent/Productpage/Productslider/Productlisting.jsx
@@ -3,38 +3,56 @@ import Syidber from "./Syidebar/Syidber";
 import Typography from "@mui/material/Typography";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Link from "@mui/material/Link";
+import Productslider from "./Productslider";
+import Productitem from "../Productitem";
 const Productlisting = () => {
   return (
-    <div className="w-11/12 mx-auto">
-      <section className="py-8">
-        <div className="container">
-          <Breadcrumbs aria-label="breadcrumb">
-            <Link
-              underline="hover"
-              color="inherit"
-              href="/"
-              className="list transition"
-            >
-              Home
-            </Link>
-            <Link
-              underline="hover"
-              color="inherit"
-              href="/"
-              className="list transition"
-            >
-              Fashion
-            </Link>
-          </Breadcrumbs>
-        </div>
-        <div className="p-3 mt-4 bg-white">
-          <div className="container flex gap-3">
-            <div className="sidebarwrapper w-[20%] h-full   bg-white">
-              <Syidber />
+    <div>
+      <div className="">
+        <section className="py-8">
+          <div className="container px-3">
+            <Breadcrumbs aria-label="breadcrumb">
+              <Link
+                underline="hover"
+                color="inherit"
+                href="/"
+                className="list transition"
+              >
+                Home
+              </Link>
+              <Link
+                underline="hover"
+                color="inherit"
+                href="/"
+                className="list transition"
+              >
+                Fashion
+              </Link>
+            </Breadcrumbs>
+          </div>
+          <div className="p-3 mt-4 bg-white">
+            <div className=" flex justify-between gap-3">
+              <div className="sidebarwrapper  w-[21%] h-full   bg-white">
+                <Syidber />
+              </div>
+              <div className="rightcontent w-[79%]">
+                <div className="grid grid-cols-4 gap-5">
+                  <Productitem />
+                  <Productitem />
+                  <Productitem />
+                  <Productitem />
+                  <Productitem />
+                  <Productitem />
+                  <Productitem />
+                  <Productitem />
+                  <Productitem />
+                  <Productitem />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   );
 };
--- src/commonent/Productpage/Productslider/Syidebar/Syidber.jsx
+++ src/commonent/Productpage/Productslider/Syidebar/Syidber.jsx
@@ -6,8 +6,16 @@ import "../Syidebar/Syidber.css";
 import { Collapse } from "react-collapse";
 import Button from "@mui/material/Button";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa6";
+import RangeSlider from "react-range-slider-input";
+import "react-range-slider-input/dist/style.css";
+// import { Rating } from "@smastrom/react-rating";
+import Rating from "@mui/material/Rating";
+import Stack from "@mui/material/Stack";
 const Syidber = () => {
   const [Open, setOpent] = useState(true);
+  const [OpenAvailability, setOpentAvailability] = useState(true);
+  const [OpenSize, setOpentOpenSize] = useState(true);
+  const [count, setCount] = useState(0);
 
   return (
     <div className="sidbear py-5">
@@ -65,6 +73,138 @@ const Syidber = () => {
             />
           </div>
         </Collapse>
+      </div>
+      <div className="box">
+        <h1 className="text-[16px] uppercase w-full font-semibold mb-3 mt-5 flex items-center justify-between">
+          Availability
+          <Button
+            className="!w-10 !min-w-10 !h-7 !rounded-full"
+            onClick={() => setOpentAvailability(!OpenAvailability)}
+          >
+            {OpenAvailability === true ? (
+              <FaAngleDown className="text-2xl " />
+            ) : (
+              <FaAngleUp className="text-2xl" />
+            )}
+          </Button>
+        </h1>
+
+        <Collapse isOpened={OpenAvailability}>
+          <div className=" px-4 relative -left-[10px] ph-3">
+            <FormControlLabel
+              control={<Checkbox size="small" />}
+              label="Available (17)"
+              className="w-full py-1"
+            />
+            <FormControlLabel
+              control={<Checkbox size="small" />}
+              label="In stock (10)"
+              className="w-full py-1"
+            />
+            <FormControlLabel
+              control={<Checkbox size="small" />}
+              label="Not available (1)"
+              className="w-full py-1"
+            />
+          </div>
+        </Collapse>
+      </div>
+      <div className="box">
+        <h1 className="text-[16px] uppercase w-full font-semibold mb-3 mt-5 flex items-center justify-between">
+          Size
+          <Button
+            className="!w-10 !min-w-10 !h-7 !rounded-full"
+            onClick={() => setOpentOpenSize(!OpenSize)}
+          >
+            {OpenSize === true ? (
+              <FaAngleDown className="text-2xl " />
+            ) : (
+              <FaAngleUp className="text-2xl" />
+            )}
+          </Button>
+        </h1>
+
+        <Collapse isOpened={OpenSize}>
+          <div className=" px-4 relative -left-[10px] ph-3">
+            <FormControlLabel
+              control={<Checkbox size="small" />}
+              label={` Small (${count})`}
+              className="w-full py-1"
+            />
+            <FormControlLabel
+              control={<Checkbox size="small" />}
+              label="Medium (5)"
+              className="w-full py-1"
+            />
+            <FormControlLabel
+              control={<Checkbox size="small" />}
+              label="Large (7)"
+              className="w-full py-1"
+            />
+            <FormControlLabel
+              control={<Checkbox size="small" />}
+              label="XL   (1)"
+              className="w-full py-1"
+            />
+            <FormControlLabel
+              control={<Checkbox size="small" />}
+              label=" XXL (3)"
+              className="w-full py-1"
+            />
+            {/* <button onClick={() => setCount(count + 1)}>hi dos</button> */}
+          </div>
+        </Collapse>
+        <h1 className="text-[16px]  w-full font-semibold mb-3 mt-5 flex items-center justify-between">
+          Filter By Price
+        </h1>
+
+        <RangeSlider />
+        <div className="flex items-center gap-5 pt-4 pb-2 ">
+          <span className="text-[13px]">
+            From: <strong>$: {100}</strong>
+          </span>
+          <span className="text-[13px]">
+            From: <strong>$: {100}</strong>
+          </span>
+        </div>
+        <h1 className="text-[16px]  w-full font-semibold mb-3 mt-5 flex items-center justify-between">
+          Filter By Rating
+        </h1>
+        {/* <Rating style={{ maxWidth: 100 }} value={5} readOnly />
+        <Rating style={{ maxWidth: 100 }} value={4} readOnly />
+        <Rating style={{ maxWidth: 100 }} value={3} readOnly />
+        <Rating style={{ maxWidth: 100 }} value={2} readOnly />
+        <Rating style={{ maxWidth: 100 }} value={1} readOnly /> */}
+        <Rating
+          name="half-rating-read"
+          defaultValue={5}
+          precision={5}
+          readOnly
+        />
+        <Rating
+          name="half-rating-read"
+          defaultValue={4}
+          precision={4}
+          readOnly
+        />
+        <Rating
+          name="half-rating-read"
+          defaultValue={3}
+          precision={3}
+          readOnly
+        />
+        <Rating
+          name="half-rating-read"
+          defaultValue={2}
+          precision={2}
+          readOnly
+        />
+        <Rating
+          name="half-rating-read"
+          defaultValue={1}
+          precision={1}
+          readOnly
+        />
       </div>
     </div>
   );
